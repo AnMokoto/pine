2dbe441bba0352a67996edcc0dab668d8d3885a6
L <chinaume@163.com>
2018-03-08 01:25:51 +0800

    fix issue

--- start.sh
+++ start.sh
@@ -1,39 +1,44 @@
 #!/bin/bash
 
-echo 'fresh beginning...'
+echo 'Fefresh beginning...'
 
-git fetch
 ver=version.txt
 
 version=`<$ver`
 echo 'local :' $version
 
+git fetch
 commit_id=`git rev-parse HEAD`
 echo 'remote:' $commit_id
 
 path=$(cd `dirname $0`;pwd)
-echo $path
-
+movedir=$1
 moveToPublish(){
-    if [ ! -n $1];then
-    	echo 'cp -rf . $1'
-    	cd $1
+    if [[ -n $movedir ]];then
+    	if [[ -d $movedir ]];then
+    		cp -rf . $movedir
+    		cd $movedir
+    	else
+    		mkdir $movedir
+    		moveToPublish
+    	fi
     fi
 }
 
 if [[ ! -n $version || $commit_id != $version ]]; then
 	echo $commit_id >$ver
 	git pull origin master
-	moveToPublish
 else
     echo 'The local same as remote commit id.'
 fi
 
+moveToPublish
 
-echo 'fresh over.it was completed.Now,ready to publish ...'
+
+echo 'Fefresh over.it was completed.Now,ready to publish ...'
 echo '----------------------------------------------------'
-echo 'sudo killall -9 uwsig'
-echo 'sudo uwsig uwsig.ini'
-echo 'sudo service nginx restart'
+echo `sudo killall -9 uwsig`
+echo `sudo uwsig uwsig.ini`
+echo `sudo service nginx restart`
 echo '----------------------------------------------------'
 echo 'Publish was completed.'
