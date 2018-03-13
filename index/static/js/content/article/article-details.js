c95f42f050865cfa628b2237fd04797fa3a75f7e
L <chinaume@163.com>
2018-03-14 01:51:12 +0800

    add js file

---
+++ index/static/js/content/article/article-details.js
@@ -0,0 +1,51 @@
+$(document).ready(function () {
+    var menustate = false
+
+    $('.article-details-nav-box div').hover(function () {
+        $(this).children().each(function () {
+            $(this).css({
+                'background-color': 'orange'
+            })
+        })
+    }, function () {
+        $(this).children().each(function () {
+            $(this).css({
+                'background-color': ''
+            })
+        })
+    }).click(function () {
+
+        $('.article-details-toc').css({
+            transform: menustate ? 'translateX(-100%)' : 'translateX(0)'
+        })
+        $(this).children().each(function (index, view) {
+            switch (index) {
+                case 0:
+                    $(this).css({
+                        'transform': menustate ? 'rotate(0deg)' : 'rotate(45deg)',
+                    })
+                    break;
+                case 1:
+                    $(this).css({
+                        'opacity': menustate ? 1 : 0,
+                    })
+                    break;
+                case 2:
+                    $(this).css({
+                        'transform': menustate ? 'rotate(0deg)' : 'rotate(-45deg)',
+                    })
+
+            }
+        })
+
+        $('.article-details-nav-box').css({
+            position: menustate ? 'absolute' : 'fixed'
+        })
+
+        menustate = !menustate
+
+    })
+
+    if ($('.toc ul li').length > 0)
+        setTimeout("$('.article-details-nav-box div').click()", 500);
+})
